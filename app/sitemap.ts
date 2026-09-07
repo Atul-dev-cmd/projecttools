8d1d6ad67fc6a3683c7cc2a596baab166d313d00
Atul Sharma <atulkumar11560@gmail.com>
2026-09-07 10:00:47 +0530

    Fix sitemap URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "http://localhost:3000",
+      url: "https://projecttools-rosy.vercel.app",
       lastModified: new Date(),
     },
   ];
